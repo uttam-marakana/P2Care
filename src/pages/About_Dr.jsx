5945744ca5ffe3ca8d66b8b4da39688b60f1e092
Uttam Marakana <uttammarakana03@gmail.com>
2025-02-19 11:46:25 +0530

    Add :: pages

---
+++ src/pages/About_Dr.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About_Dr = () => {
+  return (
+    <div>About_Dr</div>
+  )
+}
+
+export default About_Dr
---
+++ src/pages/About_Hosp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About_Hosp = () => {
+  return (
+    <div>About_Hosp</div>
+  )
+}
+
+export default About_Hosp
