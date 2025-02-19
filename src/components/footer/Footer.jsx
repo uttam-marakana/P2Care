56745cd1c46d4183a2d8b7bb80012137c7b52f58
Uttam Marakana <uttammarakana03@gmail.com>
2025-02-19 17:11:38 +0530

    update : navbar & Footer, App

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,125 +1,121 @@
 import React from "react";
-// import { Container } from 'react-bootstrap'
 import { FaPhoneAlt } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import Logo from "../../assets/img/logo1.png";
 
 function Footer() {
   return (
     <>
-      <div className="footer container">
-        <div className="d-flex">
+      <div className="footer">
+        <div className="container d-flex">
           <div className="nsettler">
             <h4 className="mb-4">Subscribe Our Newsletter</h4>
-            <p className=" fw-bold">
-              Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit,
-              sed do eiusmod tempor <br /> incididunt ut labore et eiusmod.
+            <p className=" text-wrap">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et eiusmod.
             </p>
           </div>
-          <div className="emailsec">
+          <div className="email-sect">
             <form>
               <input
                 type="email"
                 name="email"
                 id="email"
-                className="emailbtn text-light ps-5 p-3"
+                className="email-btn text-light p-2"
                 placeholder="Your Email"
               />
-              <button type="submit" className="subscribe ms-4 p-3">
+              <button type="submit" className="subscribe ms-3 p-2">
                 Subscribe
               </button>
             </form>
           </div>
         </div>
-        <hr />
-        <div className="footer_2">
-          <div className="d-flex">
-            <div className="w-25">
-              <img src="assets/img/logo1.png" alt="" />
-              <p className=" fw-bold">
-                Lorem ipsum dolor sit amet, consectet <br /> adipiscing elit,
-                sed do eiusmod tempor <br /> incididunt ut labore et dolore.
-              </p>
+        <div className="container borderLine d-flex">
+          <div className="w-25 d-flex flex-column">
+            <img src={Logo} alt="" />
+            <p className=" fw-bold">
+              Lorem ipsum dolor sit amet, consectet <br /> adipiscing elit, sed
+              do eiusmod tempor <br /> incididunt ut labore et dolore.
+            </p>
+            <div className="d-flex align-items-center">
+              <div className="i_box me-2">
+                <a href="/#">
+                  <span>
+                    <AiFillInstagram />
+                  </span>
+                </a>
+              </div>
+              <div className="i_box me-2">
+                <a href="/#">
+                  <span>
+                    <FaFacebook />
+                  </span>
+                </a>
+              </div>
+              <div className="i_box">
+                <a href="/#">
+                  <span>
+                    <FaLinkedin />
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-50">
+            <div className="d-flex justify-content-evenly">
+              <ul className="fcont fcont_1">
+                <p className="fw-bold">Quick Links</p>
+                <li>
+                  <a href="/about">About Us</a>
+                </li>
+                <li>
+                  <a href="/services">Services</a>
+                </li>
+                <li>
+                  <a href="/dr-list">Doctors</a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li>
+              </ul>
+              <ul className="fcont">
+                <p className="fw-bold">Useful Links</p>
+                <li>
+                  <a href="/#">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="/#">T & C</a>
+                </li>
+                <li>
+                  <a href="/#">Desclaimer</a>
+                </li>
+                <li>
+                  <a href="/#">FAQ</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-25">
+            <div className="cut">
               <div className="d-flex">
-                <div className="i_box me-2">
-                  <a href="/err">
-                    <span>
-                      <AiFillInstagram />
-                    </span>
+                <div className="call">
+                  <a href="/#">
+                    <FaPhoneAlt />
                   </a>
                 </div>
-                <div className="i_box me-2">
-                  <a href="/err">
-                    <span>
-                      <FaFacebook />
-                    </span>
-                  </a>
-                </div>
-                <div className="i_box">
-                  <a href="/err">
-                    <span>
-                      <FaLinkedin />
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="w-50">
-              <div className="d-flex justify-content-evenly">
-                <ul className="fcont fcont_1">
-                  <p className="fw-bold">Quick Links</p>
-                  <li>
-                    <a href="/about">About Us</a>
-                  </li>
-                  <li>
-                    <a href="/services">Services</a>
-                  </li>
-                  <li>
-                    <a href="/doctor">Doctors</a>
-                  </li>
-                  <li>
-                    <a href="/contact">Contact</a>
-                  </li>
-                </ul>
-                <ul className="fcont">
-                  <p className="fw-bold">Useful Links</p>
-                  <li>
-                    <a href="/err">Privacy Policy</a>
-                  </li>
-                  <li>
-                    <a href="/err">T & C</a>
-                  </li>
-                  <li>
-                    <a href="/err">Desclaimer</a>
-                  </li>
-                  <li>
-                    <a href="/err">FAQ</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="w-25">
-              <div className="cut">
-                <div className="d-flex">
-                  <div className="call">
-                    <a href="/err">
-                      <FaPhoneAlt />
-                    </a>
-                  </div>
-                  <div className="cut-1">
-                    <h6>Call Us Today</h6>
-                    <p className="text-dark">+91 3256523561</p>
-                  </div>
+                <div className="cut-1">
+                  <h6>Call Us Today</h6>
+                  <p className="text-dark">+91 3256523561</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <hr />
-        <p>
+        <h5 className="container borderLine pt-3">
           Copyright &copy; <em>2023.</em> All rights reserved.
-        </p>
+        </h5>
       </div>
     </>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,83 +7,97 @@ import Logo from "../../assets/img/logo2.png";
 function Navbar() {
   return (
     <>
-      <header className="">
-        <div className="w-100 ">
-          <a href="mailto:" className="display-flex justify-content-end">
-            <IoIosMail />
-            contact123@gmail.com
-          </a>
+      {/* <header className=""> */}
+      {/* //---- Banner Start ----//*/}
+      <div className="w-100 ">
+        <a href="mailto:" className="">
+          <IoIosMail className="mx-1 " />
+          contact123@gmail.com
+        </a>
+      </div>
+      <div className="d-flex px-5 justify-content-between padd">
+        <img src={Logo} alt="logo" width="163px" height="90px" />
+        <div className="contact align-content-center">
+          <div className="d-flex">
+            <div className="mail">
+              <p>Send Us Mail</p>
+              <a href="mailto:" className="text-dark">
+                <IoIosMail />
+                contact123@gmail.com
+              </a>
+            </div>
+            <div className="contact_number ms-5">
+              <p>Call To Us</p>
+              <a href="tel:+" className="text-dark">
+                <FaPhoneAlt />
+                01234569872
+              </a>
+            </div>
+          </div>
         </div>
+      </div>
+      {/* //---- Banner End ----//*/}
 
-        <div className="d-flex px-5 justify-content-between ">
-          <img src={Logo} alt="logo" width="163px" height="90px" />
-          <div className="contact align-content-center">
-            <div className="d-flex">
-              <div className="mail">
-                <p>Send Us Mail</p>
-                <a href="mailto:" className="text-dark">
-                  <IoIosMail />
-                  contact123@gmail.com
-                </a>
-              </div>
-              <div className="contact_number ms-5">
-                <p>Call To Us</p>
-                <a href="tel:+" className="text-dark">
-                  <FaPhoneAlt />
-                  01234569872
-                </a>
-              </div>
-            </div>
+      {/* //---- Navbar Start ----// */}
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className=" container-fluid ">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="container navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item ">
+                <Link
+                  className="nav-link active text-dark"
+                  aria-current="page"
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item ps-3">
+                <Link className="nav-link text-dark" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item ps-3">
+                <Link className="nav-link text-dark" to="/dr-profile">
+                  Doctor
+                </Link>
+              </li>
+              <li className="nav-item ps-3">
+                <Link className="nav-link text-dark" to="/services">
+                  Service_Areas
+                </Link>
+              </li>
+              <li className="nav-item ps-3">
+                <Link className="nav-link text-dark" to="/blogs">
+                  Blog
+                </Link>
+              </li>
+              <li className="nav-item ps-3">
+                <Link className="nav-link text-dark" to="/book-appointment">
+                  Book_Appointment
+                </Link>
+              </li>
+              <li className="nav-item ps-3">
+                <Link className="nav-link text-dark" to="/hospital">
+                  Contact Us{" "}
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-        <nav className="navbar container-fluid navbar-expand-lg bg-body-tertiary  ">
-          <div className="container-fluid">
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link
-                    className="nav-link active text-dark"
-                    aria-current="page"
-                    to="/"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-dark" to="/about">
-                    About
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-dark" to="/doctoreprofile">
-                    Doctor
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-dark" to="/services">
-                    Service Areas
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-dark" to="/blog">
-                    Blog
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-dark" to="/bookAppointment">
-                    Book Appointment
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-dark" to="/hospital">
-                    Contact Us{" "}
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
+      </nav>
+      {/* //---- Navbar End ----// */}
     </>
   );
 }
