d6b472623c8fb02b05cddf4ceedcbff02a510049
Uttam Marakana <uttammarakana03@gmail.com>
2025-03-05 16:04:58 +0530

    Update :: Code

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
-  )
-}
+    <div className="container">
+      <div className="review text-center">
+        <p className="review-cont">
+          Lorem ipsum dolor sit amet, consectet adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor
+          sit amet, consectet adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </p>
+        <div className="row justify-content-center align-items-center mt-4">
+          <div className="col-auto">
+            <img
+              src="assets/img/testimonial.png"
+              width="77px"
+              height="77px"
+              alt="Testimonial"
+            />
+          </div>
+          <div className="col-auto text-start">
+            <h5 className="text-info mb-0">Leela Rogers</h5>
+            <p className="fs-6">Businessman</p>
+          </div>
+        </div>
+      </div>
 
-export default Reviews
+      {/* Navigation Buttons */}
+      <div className="d-flex justify-content-end mt-4">
+        <button className="indicator btn btn-light me-3">
+          <FaChevronLeft />
+        </button>
+        <button className="indicator btn btn-light">
+          <FaChevronRight />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
