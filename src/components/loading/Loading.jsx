69175350be37bcf9705de11290977db8ae4ab699
Uttam Marakana <uttammarakana03@gmail.com>
2025-03-06 12:37:26 +0530

    Update : : Code

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <h3>Preparing your experience...</h3>
+      <p>Please wait while we load the content for you.</p>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
